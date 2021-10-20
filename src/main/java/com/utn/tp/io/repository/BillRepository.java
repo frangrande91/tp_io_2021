804296b06720701155f78499d635e096b4ba23dd
NahuelSalomon <nahuelmdp@hotmail.com>
2021-10-20 18:18:58 -0300

    Changes in ABM bill

--- src/main/java/com/utn/tp/io/repository/BillRepository.java
+++ src/main/java/com/utn/tp/io/repository/BillRepository.java
@@ -1,4 +1,9 @@
 package com.utn.tp.io.repository;
 
-public class BillRepository {
+import com.utn.tp.io.model.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillRepository extends JpaRepository<Bill,Integer> {
 }
